bc33ec3e0b66f3af1e6a052be4ff61e14f6dbabf
Catrina Alexandru <ovidiualex.catrina@gmail.com>
2022-03-14 15:14:40 +0200

    other commit

--- src/main/java/ro/mycode/librarmanagerapi/web/BookController.java
+++ src/main/java/ro/mycode/librarmanagerapi/web/BookController.java
@@ -3,6 +3,7 @@ package ro.mycode.librarmanagerapi.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ro.mycode.librarmanagerapi.model.Book;
@@ -11,6 +12,7 @@ import ro.mycode.librarmanagerapi.repository.BookRepository;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class BookController {
 
 
@@ -27,7 +29,5 @@ public class BookController {
 
         return  new ResponseEntity<>(bookRepository.findAll(), HttpStatus.ACCEPTED);
     }
-
-
 
 }
